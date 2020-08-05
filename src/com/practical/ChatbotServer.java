6d820f72e1676dc4a38d282f051484f3841b52b5
Marnus Coetzee <marnuscoe@gmail.com>
2020-08-05 12:50:57 +0200

    finished client server

--- src/com/practical/ChatbotServer.java
+++ src/com/practical/ChatbotServer.java
@@ -1,8 +1,28 @@
 package com.practical;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ChatbotServer {
 
+    private static ServerSocket serverSocket;
+    private final static int PORT = 8080;
+
     public static void main(String[] args) {
-	// write your code here
+        try {
+            System.out.println("Server attempting to bind to port: " + PORT);
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Waiting for connections...\n");
+
+            do {
+                Socket clientConnection = serverSocket.accept();
+                ChatbotServerHandler serverHandler = new ChatbotServerHandler(clientConnection);
+                Thread t = new Thread(serverHandler);
+                t.start();
+            } while (true);
+        } catch (IOException e) {
+            System.err.println("Unable to attach to a port");
+        }
     }
 }
--- src/com/practical/ChatbotServerHandler.java
+++ src/com/practical/ChatbotServerHandler.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
+import java.util.StringTokenizer;
 
 public class ChatbotServerHandler implements Runnable {
 
@@ -20,8 +22,79 @@ public class ChatbotServerHandler implements Runnable {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader((clientConnection.getInputStream())));
             PrintWriter out = new PrintWriter(clientConnection.getOutputStream(), true);
-        } catch (IOException e) {
 
+            int responseNumber = 0;
+            int numQuestions = 0;
+            out.println("HELLO - You may ask me 4 questions");
+            boolean sessionStarted = false;
+            String message = "";
+
+            while ((numQuestions < 4) && (!message.equals("DONE"))) {
+                message = in.readLine();
+                StringTokenizer tokenizer = new StringTokenizer(message);
+                String command = tokenizer.nextToken();
+                System.out.println("command: " + command);
+                if (command.equals("HELLO") && tokenizer.nextToken().equals("BOT")) {
+                    sessionStarted = true;
+                    System.out.println("Client started a session");
+                    out.println("Ask me a question or done");
+                } else if (command.equals("ASK") && sessionStarted) {
+                    numQuestions += 1;
+                    responseNumber += 1;
+                    String question = "";
+                    while (tokenizer.hasMoreTokens()) {
+                        question += tokenizer.nextToken();
+                    }
+                    System.out.println("Question received: " + question);
+                    if (question.contains("virus") || question.contains("covid-19")) {
+                        out.println("0" + responseNumber + "Please see sacoronavirus.co.za");
+                    } else if (question.startsWith("Are")) {
+                        Random r = new Random();
+                        int randomResponse = r.nextInt(3);
+                        switch (randomResponse) {
+                            case 0: out.println("0" + responseNumber + "Yes");
+                                break;
+                            case 1: out.println("0" + responseNumber + "No");
+                                break;
+                            case 2: out.println("0" + responseNumber + "Maybe");
+                                break;
+                        }
+                    } else if (question.startsWith("Why")) {
+                        out.println("0"+responseNumber+"Because the boss says so - see Ulink");
+                    } else {
+                        Random r = new Random();
+                        int decision = r.nextInt(3);
+                        switch(decision)
+                        {
+                            case 0: out.println("0" + responseNumber + "Escusez Moi?");
+                                break;
+                            case 1: out.println("0" + responseNumber + "Oh ok!");
+                                break;
+                            case 2:out.println("0" + responseNumber + "Meh");
+                                break;
+                        }
+                    }
+                    System.out.println(numQuestions + " questions asked so far");
+                } else if (!message.equals(("DONE"))) {
+                    out.println("The command you entered is incorrect");
+                }
+                if (message.equals("DONE")) {
+                    out.println("0" + responseNumber + "OK BYE - "+numQuestions + " questions answered");
+                } else {
+                    out.println("HAPPY TO HAVE HELPED - "+numQuestions+" Questions answered");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                System.out.println("Closing conenction");
+                if (clientConnection != null) {
+                    clientConnection.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
